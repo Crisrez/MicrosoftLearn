2135dfa23b29e6ef7f6a69f9b268450f6ace1d7a
Cristian - Luhmann <99039961+Crisrez@users.noreply.github.com>
2024-05-28 15:05:42 -0300

    For y While1
    Carga de los ejercicios completos de For y primero de While

---
+++ For/Program.cs
@@ -0,0 +1,10 @@
+﻿for (int i = 1; i <= 100; i++)
+{
+    string categoria = " - ";
+    
+    if (i % 3 == 0) categoria += "Fizz";
+    if (i % 5 == 0) categoria += "Buzz";
+
+    if (categoria == " - ") Console.WriteLine($"{i}");
+    else Console.WriteLine($"{i}{categoria}");
+}
---
+++ While/Program.cs
@@ -0,0 +1,24 @@
+﻿Random dice = new();
+
+int heroHealth = 10;
+int monsterHealth = 10;
+
+do
+{
+    int damage = dice.Next(10);
+    monsterHealth -= damage;
+    Console.WriteLine($"Monster was damaged and lost {damage} health and now has {monsterHealth} health.");
+
+    if (monsterHealth > 0)
+    {
+        damage = dice.Next(10);
+        heroHealth -= damage;
+        Console.WriteLine($"Hero was damaged and lost {damage} health and now has {monsterHealth} health.");
+    }
+    
+} while (heroHealth > 0 && monsterHealth > 0);
+
+if (heroHealth > 0)
+    Console.WriteLine("Hero wins!");
+else if (monsterHealth > 0)
+    Console.WriteLine("Mosnter wins!");
